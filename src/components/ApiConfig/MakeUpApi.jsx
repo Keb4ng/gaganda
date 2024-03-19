f441ff15e50f10b4aac35d97150fd27ad96b0165
Jon Trinidad <122079605+Keb4ng@users.noreply.github.com>
2024-03-19 09:47:36 +0800

    Update MakeUpApi.jsx
    sheesh

--- src/components/ApiConfig/MakeUpApi.jsx
+++ src/components/ApiConfig/MakeUpApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const MakeUpApi = axios.create({
-  baseURL: "http://makeup-api.herokuapp.com/api/v1",
+  baseURL: "https://makeup-api.herokuapp.com/api/v1",
 });
 
 export const getData = async () => {
